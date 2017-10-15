d70a3e07281f5e8eeecb5030dc3e906bf2984cd3
Dario Vigil <dariovigil@gmail.com>
2017-10-15 19:08:08 +0200

    Added buttons

--- js/index-min.js
+++ js/index-min.js
@@ -2,9 +2,12 @@
 const quoteContainer = document.querySelector('.quote-text');
 const newQuoteBtn = document.querySelector('.new-quote-btn');
 const authorContainer = document.querySelector('.quote-author');
+const twitterBtn = document.querySelector('.tweet-quote-btn');
 newQuoteBtn.addEventListener('click', getQuote);
-googleCustomSearchAPIkey = 'AIzaSyDBm5H1wNfavWFdiEVk2i1mSePURSBnwIo';
+twitterBtn.addEventListener('click', tweetQuote);
 const url = "http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
+const currentQuote = [];
+const currentAuthor = [];
 //
 // REQUEST API WITH FETCH
 //
@@ -35,7 +38,6 @@ function getQuote() {
     url: url,
     cache: false,
     success: function(data) {
-      console.log(data);
       renderQuote(data);},
   });
 }
@@ -44,5 +46,11 @@ function renderQuote(quote) {
   quoteContainer.innerHTML = quote[0].content;
   authorContainer.innerHTML = `- ${quote[0].title}`;
 }
+function openURL(url){
+  window.open(url, 'Share', 'width=650, height=500, toolbar=0, scrollbars=1 ,location=0 ,statusbar=0,menubar=0, resizable=0');
+}
+function tweetQuote(){
+  openURL('https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' + encodeURIComponent('"' + quoteContainer.innerText + '" ' + authorContainer.innerHTML));
+    }
 
 
